ba8aa194d5d8ebdc608de37b330614dddb518bb1
Will Hamilton <whamilton@exeter.edu>
2018-11-08 14:36:18 -0500

    New GOG Api Strat #2

--- AppAPI/controllers/games.js
+++ AppAPI/controllers/games.js
@@ -78,6 +78,7 @@ module.exports.getGOGPrice = function(req,res) {
 		G.findOne({'appid' : req.params.appid})
 			.exec(function(err, game) {
 				if (err) { throw err };
+				console.log(game.name)
 				if(!game) {
 					sendJsonResponse(res, 404, {
 						"message": "game id not found"
