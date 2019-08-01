83b8adb0de237e01e4f914ce2c46c59f5bdd2079
Will Hamilton <will@Wills-MacBook-Pro.local>
2019-08-01 13:46:44 -0400

    Added Proto-Sitemap 5

--- AppServer/controllers/ctrl.js
+++ AppServer/controllers/ctrl.js
@@ -128,7 +128,7 @@ module.exports.game = function(req, res) {
 	request(requestOptions, function(err, response, body) {
 		if (req.params.appid.length < 10) {
 			request({ url: 'https://store.steampowered.com/api/appdetails?appids=' + req.params.appid , method: "GET", json: {} }, function(err, response, steamBody) {
-				if (steamBody[req.params.appid].data) { renderGame(req, res, body, "$" + steamBody[req.params.appid].data.price_overview.final / 100); } });
+				if (steamBody[req.params.appid].data.price_overview) { renderGame(req, res, body, "$" + steamBody[req.params.appid].data.price_overview.final / 100); } });
 		} else {
 			renderGame(req, res, body, "Not Sold Here")
 		}
